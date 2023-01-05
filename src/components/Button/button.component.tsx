e3ab260604c2dd14b3d07c627ea64883986ea8a0
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2023-01-05 17:28:09 -0600

    feat(button): custom button component

--- src/components/Button/button.component.tsx
+++ src/components/Button/button.component.tsx
@@ -2,11 +2,12 @@ import './button.styles.scss';
 
 interface ButtonProps {
   text: string;
+  onClick: () => void;
 }
 
-const Button = ({ text }: ButtonProps) => {
+const Button = ({ text, onClick }: ButtonProps) => {
   return (
-    <button className="button" type="button">
+    <button className="button" type="button" onClick={onClick}>
       {text}
     </button>
   );
